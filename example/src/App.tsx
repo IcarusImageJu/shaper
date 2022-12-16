e35143c93ef6792f24def1535fcc81180ab0857c
Julien Bonte <julien.bonte@keepeek.com>
2022-12-16 20:22:44 +0100

    feat(init): reinit bob

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,26 +1,8 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
 import Shaper from '@julienbonteweb/shaper';
 
 const App = () => {
-  return (
-    <View style={styles.container}>
-      <Shaper />
-    </View>
-  );
+  return <Shaper />;
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
-});
